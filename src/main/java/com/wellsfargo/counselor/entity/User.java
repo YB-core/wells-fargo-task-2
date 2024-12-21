d0db7b79cbc3f71d1294832a393376dad5bd34aa
YB-core <yashbhopale00@gmail.com>
2024-12-21 22:50:26 +0530

    Implemented data model entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/User.java
@@ -0,0 +1,60 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Order;
+
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+
+    // Default constructor (required by JPA)
+    public User() {}
+
+    // Constructor initializing all variables except id
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
